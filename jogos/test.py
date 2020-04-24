ac7ec0db95f9da9187a033685c939165363f66df
dduraess <davison@Davisons-Mac-mini.local>
2020-04-24 16:11:10 -0300

    informar pasta, format impressao

--- jogos/test.py
+++ jogos/test.py
@@ -4,14 +4,15 @@ import csv
 
 cnpjs = []
 cpfs = []
-pasta = '/home/davison/Downloads/'
+pasta = input("Informe a pasta: ")
 arquivos = [f for f in os.listdir(pasta) if re.match(r'^elementos-carga.*csv$', f)]
 
 for nome_arquivo in arquivos:
     with open(pasta + nome_arquivo, "r") as arquivo:
         csv_reader = csv.reader(arquivo)
+        csv_reader.__next__
         for linha in csv_reader:
             if linha[2] == "CNPJ":
-                print(linha[1])
+                print("({}, {}), ".format(linha[1][:7], linha[1][8:12]), end='')
             elif linha[2] == "CPF":
-                print(linha[1])
+                print("{}, ".format(linha[1][:8]), end='')
